87ee6bd0100c3a11f831f523f029865af26233ea
Johnthadeouz <tadeo.calles@outlook.com>
2025-11-05 00:38:40 -0500

    Added expansive wave particle, orthogonal view when combating, and ship's attack functionality.

--- Scripts/siren_area_3d.gd
+++ Scripts/siren_area_3d.gd
@@ -1,15 +1,18 @@
 extends Area3D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var siren_in_range:Siren = null
 
 
 func _on_siren_entered(siren: Siren) -> void:
-	siren.start_attacking(get_parent())
+	var ship:Ship = get_parent()
+	siren_in_range = siren
+	siren.start_attacking(ship)
+	await get_tree().create_timer(1.0).timeout
+	if siren_in_range and not ship.under_attack:
+		ship.under_attack = true
+
+
+
+func _on_body_exited(siren: Siren) -> void:
+	var ship:Ship = get_parent()
+	siren.chase_ship(get_parent())
+	siren_in_range = null
