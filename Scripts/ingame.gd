260d8b13c9c6ac7ad38035e9c0757ef901e46b4f
LucarioXx <lucadonner128@gmail.com>
2025-11-09 14:34:41 +0100

    Update ingame.gd

--- Scripts/ingame.gd
+++ Scripts/ingame.gd
@@ -9,5 +9,5 @@ func _process(delta: float) -> void:
 
 
 func _on_ship_package_collected(collected_package: Package) -> void:
-	var attach_marker = $Ship.get_node("Marker3D") # Prüfe den Pfad!
+	var attach_marker = $Ship.get_node("Marker3D")
 	collected_package.pickup($Ship, attach_marker)
